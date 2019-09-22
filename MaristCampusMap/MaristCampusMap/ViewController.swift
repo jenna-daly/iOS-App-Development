b3c25212dce4cb34f9a8780cee0dff36c802eb96
Jenna Daly <jennadaly@148-100-137-14.FoxNet.marist.edu>
2019-09-22 18:11:59 -0400

    Setting up Xcode

---
+++ MaristCampusMap/MaristCampusMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaristCampusMap
+//
+//  Created by Jenna Daly on 9/22/19.
+//  Copyright © 2019 Jenna Daly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
