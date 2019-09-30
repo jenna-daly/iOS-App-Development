9e260d6cc0c19cde32b7fe75aa089d1290d54b66
Jenna Daly <jennadaly@148-100-137-14.FoxNet.marist.edu>
2019-09-30 12:42:26 -0400

    Built basic UI

--- MaristCampusMap/MaristCampusMap/ViewController.swift
+++ MaristCampusMap/MaristCampusMap/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ViewHolder: UIView!
+    @IBOutlet weak var MapArrow: UIImageView!
+    @IBOutlet weak var Nav: UINavigationBar!
+    @IBOutlet weak var SelectLocationBTN: UIBarButtonItem!
+    @IBOutlet weak var AddLocationBTN: UIBarButtonItem!
+    @IBOutlet weak var AddLocationView: UIView!
+    @IBOutlet weak var LocationList: UIPickerView!
+    @IBOutlet weak var WelcomeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func AddLocationPressed(_ sender: Any) {
+        AddLocationView.isHidden = false
+        MapArrow.isHidden = true
+        WelcomeImage.isHidden = true
+    }
+    
+    @IBAction func SelectLocationPressed(_ sender: Any) {
+        AddLocationView.isHidden = true
+        MapArrow.isHidden = true
+        LocationList.isHidden = false
+        WelcomeImage.isHidden = true
+    }
 }
 
