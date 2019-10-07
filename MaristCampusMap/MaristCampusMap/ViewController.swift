d492b391d48a06fe13c990ed8ff3c6887d2377b9
Jenna Daly <jennadaly@148-100-137-14.FoxNet.marist.edu>
2019-10-06 21:46:53 -0400

    Tried to implement JSON reading to use in UIPickerView/as DB, not yet successful

--- MaristCampusMap/MaristCampusMap/ViewController.swift
+++ MaristCampusMap/MaristCampusMap/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
     
     let locationManagement : CLLocationManager = CLLocationManager()
 
@@ -22,6 +22,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var LocationList: UIPickerView!
     @IBOutlet weak var WelcomeImage: UIImageView!
     @IBOutlet weak var EnterNewLocation: UITextField!
+    @IBOutlet weak var HomeBTN: UIBarButtonItem!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +32,65 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManagement.delegate = self
         locationManagement.requestWhenInUseAuthorization()
         locationManagement.startUpdatingLocation()
+        
+        guard let path = Bundle.main.path(forResource: "MaristLocations", ofType: "json") else {return}
+        let url = URL(fileURLWithPath: path)
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        if error != nil{
+            print("error")
+        }
+        else{
+            if let content = data {
+                do{
+                    let myJSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    if let locs = myJSON["Name"] as? NSDictionary{
+                        print(locs)
+                    }
+                }
+                catch{}
+            }
+            }
+        }
+        task.resume()
+        
+        /*do{
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            print(json)
+            guard let array = json as? [Any] else {return}
+            
+            for user in array {
+                guard let userDict = user as? [String: Any] else {return}
+                guard let userLoc = userDict["Name"] as? String else {return}
+                guard let userLat = userDict["Lat"] as? Int else {return}
+                guard let userLong = userDict["Long"] as? Int else {return}
+                
+                print(userLoc)
+                print(userLat)
+                print(userLong)
+            }
+        }
+        catch {
+            print("error")
+        }*/
     }
+    
+    let userDict = ["Hancock", "Donnelly", "Lowell Thomas", "Dyson", "Fontaine"]
+    func numberOfComponents(in LocationList : UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ LocationList : UIPickerView, titleForRow row : Int, forComponent component : Int) -> String? {
+        return userDict[row]
+    }
+    
+    func pickerView(_ LocationList : UIPickerView, numberOfRowsInComponent component : Int) -> Int {
+        return userDict.count
+    }
+
+    
+   
+    
 
     @IBAction func AddLocationPressed(_ sender: Any) {
         AddLocationView.isHidden = false
@@ -43,6 +103,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         MapArrow.isHidden = true
         LocationList.isHidden = false
         WelcomeImage.isHidden = true
+    }
+    @IBAction func HomeButtonPressed(_ sender: Any) {
+        AddLocationView.isHidden = true
+        MapArrow.isHidden = true
+        LocationList.isHidden = true
+        WelcomeImage.isHidden = false
+        
     }
 }
 
